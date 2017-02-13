bb486ec81e2eb931f677a2786eba64f74bb89302
SupachaiChaipratum <ch.supachai.aon@gmail.com>
2017-02-13 19:10:15 +0700

    add non type

--- intro/helloworld.js
+++ intro/helloworld.js
@@ -1,4 +1,6 @@
 var say = "Hello, World";
 console.log(say); // "Hello, World"
 say = "Hello HOI";
-say = 555; // compile error 
+// say = 555 // compile error 
+var hello = "The HOI Stroy";
+hello = 123;
--- intro/helloworld.ts
+++ intro/helloworld.ts
@@ -1,9 +1,13 @@
 
-    let say:String = "Hello, World"
+let say:String = "Hello, World"
 
-    console.log(say) // "Hello, World"
+console.log(say) // "Hello, World"
 
-    say = "Hello HOI" 
+say = "Hello HOI" 
 
-    say = 555 // compile error 
+// say = 555 // compile error 
+
+let hello = "The HOI Stroy"
+
+hello = 123
 
