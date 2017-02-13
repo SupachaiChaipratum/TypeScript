1e1991968909be5a21a9c865cfa60f77761cc788
SupachaiChaipratum <ch.supachai.aon@gmail.com>
2017-02-13 19:58:03 +0700

    add tuple type

--- intro/basicTypes.js
+++ intro/basicTypes.js
@@ -11,5 +11,11 @@ says = "Hello HOI";
 //Array
 var nums = [1, 2, 3];
 var numList = [1, 2, 3];
-var num = [1, 2, "test"];
-var studentAndScore;
+var num = [1, 2, 3];
+// let nums2Type: number[] = [1,"1"]; // compile error
+// let numList2Type: Array<number> = [2,"2"]; // compile error
+// let num2Type = [3,"3"]; // pass
+// Tuple
+var man;
+man = ["HOI", 24];
+// man = [24 , "HOI"] // Type 'number' is not assignable to type 'string'.  
--- intro/basicTypes.ts
+++ intro/basicTypes.ts
@@ -17,6 +17,17 @@ let nums: number[] = [1, 2, 3];
 
 let numList: Array<number> = [1, 2, 3];
 
-let num  = [1, 2, "test"];
+let num  = [1, 2, 3];
 
-let studentAndScore: [string, number,string]
+
+// let nums2Type: number[] = [1,"1"]; // compile error
+
+// let numList2Type: Array<number> = [2,"2"]; // compile error
+
+// let num2Type = [3,"3"]; // pass
+
+// Tuple
+let man: [string, number];
+
+man = ["HOI", 24]
+// man = [24 , "HOI"] // Type 'number' is not assignable to type 'string'. 
