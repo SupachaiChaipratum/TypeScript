7ddb9884c737ce56f7263b562444d40e12e479ce
SupachaiChaipratum <ch.supachai.aon@gmail.com>
2017-02-16 23:09:47 +0700

    add Rest Parameters

--- intro/function.ts
+++ intro/function.ts
@@ -46,8 +46,22 @@ let result2 = buildName("Bob", undefined);       // still works, also returns "B
 let result4 = buildName("Bob", "Adams");         // ah, just right
 
 
+function buildName(firstName = "Will", lastName: string) {
+    return firstName + " " + lastName;
+}
+
+let result1 = buildName("Bob");                  // error, too few parameters
+let result2 = buildName("Bob", "Adams", "Sr.");  // error, too many parameters
+let result3 = buildName("Bob", "Adams");         // okay and returns "Bob Adams"
+let result4 = buildName(undefined, "Adams");     // okay and returns "Will Adams"
 
 
+// Rest Parameters
+function buildName(firstName: string, ...restOfName: string[]) {
+    return firstName + " " + restOfName.join(" ");
+}
+
+let employeeName = buildName("Joseph", "Samuel", "Lucas", "MacKinzie");
 
 
 
