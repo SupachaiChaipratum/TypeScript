108b4c6f6d544938544c7bbfc08150d5375847c5
SupachaiChaipratum <ch.supachai.aon@gmail.com>
2017-02-16 22:54:51 +0700

    // Optional Parameters

--- intro/function.ts
+++ intro/function.ts
@@ -23,3 +23,30 @@ function bar(ex){
     return "ex";
 }
 
+// Optional Parameters
+function human(name: string, age?: string): void {
+    // ..
+}
+
+
+// Overloads
+function padding(all: number);
+function padding(topAndBottom: number, leftAndRight: number);
+function padding(top: number, right: number, bottom: number, left: number);
+
+// Actual implementation that is a true representation of all the cases the function body needs to handle
+function padding(a: number, b?: number, c?: number, d?: number) {
+    if (b === undefined && c === undefined && d === undefined) {
+        b = c = d = a;
+    }
+    else if (c === undefined && d === undefined) {
+        c = a;
+        d = b;
+    }
+    return {
+        top: a,
+        right: b,
+        bottom: c,
+        left: d
+    };
+}
