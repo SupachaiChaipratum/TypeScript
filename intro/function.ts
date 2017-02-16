052e4bd5b7a2c2cbdb82735a7b65bfdeec0056fa
SupachaiChaipratum <ch.supachai.aon@gmail.com>
2017-02-16 23:06:01 +0700

    Default Parameters

--- intro/function.ts
+++ intro/function.ts
@@ -31,3 +31,50 @@ function human(fname: string,lname:string, age?: number): void {
 human("HOI","Stroy",25) // ok
  // human("HOI") // error
 
+
+
+
+
+//====
+
+// Default Parameters
+function buildName(firstName: string, lastName = "Smith") {
+    return firstName + " " + lastName;
+}
+let result1 = buildName("Bob");                  // works correctly now, returns "Bob Smith"
+let result2 = buildName("Bob", undefined);       // still works, also returns "Bob Smith"
+let result4 = buildName("Bob", "Adams");         // ah, just right
+
+
+
+
+
+
+
+// Overloads
+function padding(all: number);
+function padding(topAndBottom: number, leftAndRight: number);
+function padding(top: number, right: number, bottom: number, left: number);
+// Actual implementation that is a true representation of all the cases the function body needs to handle
+function padding(a: number, b?: number, c?: number, d?: number) {
+    if (b === undefined && c === undefined && d === undefined) {
+        b = c = d = a;
+    }
+    else if (c === undefined && d === undefined) {
+        c = a;
+        d = b;
+    }
+    return {
+        top: a,
+        right: b,
+        bottom: c,
+        left: d
+    };
+}
+
+padding(1); // Okay : all
+padding(1,1); // Okay : topAndBottom, leftAndRight
+padding(1,1,1,1); // Okay : top, right, bottom, left
+
+//padding(1,1,1); // Error: Not a part of the available overloads
+
