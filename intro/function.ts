a8bcd8d8f7d31fb289f447b1c844b176114cb5eb
SupachaiChaipratum <ch.supachai.aon@gmail.com>
2017-02-16 22:36:57 +0700

    add ex

--- intro/function.ts
+++ intro/function.ts
@@ -6,3 +6,20 @@ function nameOfFunction() {
 // anonymous function
 let func = function() { return "some things"; };
 
+
+// function has type
+function foo(sample: number) : number {
+    return sample;
+}
+
+foo(1) // ok
+// foo("1") // error
+
+let num1 = foo(2); // ok
+// num1 = "2"; // error
+
+function bar(ex){
+
+    return "ex";
+}
+
