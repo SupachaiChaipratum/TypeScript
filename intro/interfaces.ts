c11d80d4dc867bef44182a56b5ad6444cff9d828
SupachaiChaipratum <ch.supachai.aon@gmail.com>
2017-02-18 23:29:59 +0700

    add implement class

--- intro/interfaces.ts
+++ intro/interfaces.ts
@@ -8,5 +8,9 @@ function printMyBook(myBook: Book) {
     console.log(myBook.title);
 }
 
-let myBook = {title: "Harry", author: "JK" , numpages:300};
+let myBook : Book= {title: "Harry", author: "JK" , numpages:300};
 printMyBook(myBook);
+
+class MyBook implements Book {
+   
+}
