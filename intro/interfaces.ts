23166b530e796be613c036d7be9c8911b7219bb9
SupachaiChaipratum <ch.supachai.aon@gmail.com>
2017-02-18 23:37:03 +0700

    Optional Properties

--- intro/interfaces.ts
+++ intro/interfaces.ts
@@ -8,9 +8,24 @@ function printMyBook(myBook: Book) {
     console.log(myBook.title);
 }
 
+
+
+
+// Optional Properties
+interface Car {
+    brand:string
+    color?: string;
+}
+
+
+
 let myBook : Book= {title: "Harry", author: "JK" , numpages:300};
 printMyBook(myBook);
 
 class MyBook implements Book {
-   
+    title: string;
+    author: string;
+    numpages: number;
+
+    asasas:number;
 }
